c6ad5aa9aa2c819614a89693e1143833d36008fc
Joen Tan <joentze01@gmail.com>
2024-10-26 14:52:50 +0800

    feat: added property class

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# singularity
+# Singularity
 Converting unstructured data into structured formats
+
+# Parsing Methods
+
+The following approach allows for text on scanned documents to be extracted
+
+```
+file > pdf > image > llm > formatted output
+```
+
+```
+file > pdf > image > llm > markdown > llm > formatted output
+```
+
+The following approach allows for less LLM-reliant method of extracted information
+
+```
+file > unstructured-parser > llm > markdown > llm > formatted output
+```
---
+++ handlers/schema/models/Property.py
@@ -0,0 +1,28 @@
+"""Model for extracted property"""
+from typing import Optional, Literal, List
+from pydantic import BaseModel
+from openai.types.chat.chat_completion_function_call_option_param import ChatCompletionFunctionCallOptionParam
+
+
+class Property(BaseModel):
+    """
+    type (string): type of property
+    name (string): name of property
+    description (string): description of property
+    sub_property (Property): sub property, if type is array, or dictionary,
+                            declare it here
+    """
+    type: Literal["string"] | Literal["float"] | Literal["integer"] | Literal["enum"]
+    name: str
+    description: str
+    enum_options: Optional[List[str]] = []
+
+    def validate_sub_property(self):
+        if self.type in ["enum"] and len(self.enum_options) == 0:
+            raise ValueError(
+                f"An enum value must be defined when type is '{self.type}'.")
+
+    # Ensure the validation is called after initialization
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.validate_sub_property()
